239a8c42c224570bc00411bb94defc009e0f2858
Nita Chaudhari <nitachaudhari1997@gmail.com>
2025-04-01 17:35:41 +0530

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Quiz from "./pages/Quiz";
-import Stories from "./pages/Stories";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function App() {
-  return (
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/stories" element={<Stories />} />
-        </Routes>
-      </Router>
-    </ChakraProvider>
-  );
-}
-
-export default App;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
