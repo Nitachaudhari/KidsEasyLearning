64368574a845c9729d7063750a088716a85d1b18
Nita Chaudhari <nitachaudhari1997@gmail.com>
2025-04-23 13:52:39 +0530

    stories load

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,11 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://kidslearneasy.netlify.app"
-];
+const cors = require("cors");
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
 
